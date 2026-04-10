fee4afd3a85151a936e0a3efe33f21077ab88645
Keshav Sharma <158580043+Keshav-byt@users.noreply.github.com>
2026-04-10 18:26:17 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,8 +3,6 @@ import './App.css'
 import Landing from "./Components/Landing.jsx";
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import SoilAnalysis from "./Components/SoilAnalysis.jsx";
-import WeatherPrediction from "./Components/WeatherPrediction.jsx";
-import PestDetection from "./Components/PestDetection.jsx";
 import GovtSchemes from "./Components/GovtSchemes.jsx";
 
 function App() {
@@ -19,13 +17,6 @@ function App() {
             element: <SoilAnalysis/>
         },
         {
-            path:"/weather",
-            element: <WeatherPrediction/>
-        },
-        {
-            path:"/pest",
-            element:<PestDetection/>
-        },{
             path:"/govt-schemes",
             element: <GovtSchemes/>
         }
